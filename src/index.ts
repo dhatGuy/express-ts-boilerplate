43e08b2fb3f7f1aa28eee9aa917108daf2bcd32d
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-03-22 10:23:56 +0100

    🚨 setup linting

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ if (!process.env.PORT) {
   process.exit(1);
 }
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
+const PORT: number = parseInt(process.env.PORT , 10);
 
 const app = express();
 
