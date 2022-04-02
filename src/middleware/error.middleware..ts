ea5963f153157191cc25f76161272278b61c4d56
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-04-02 16:42:00 +0100

    🥅 : tsoa error validation

--- src/middleware/error.middleware..ts
+++ src/middleware/error.middleware..ts
@@ -1,16 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import { ValidateError } from "tsoa";
 import HttpException from "../common/http-exception";
 import { HttpStatus } from "../interfaces/httpStatus";
 import logger from "../utils/logger";
 
 export const errorMiddleware = (
   error: HttpException,
-  _req: Request,
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
   const { statusCode, message, status, errors } = error;
   logger.error(error);
+
+  if (error instanceof ValidateError) {
+    logger.warn(`Caught Validation Error for ${req.path}:`, error.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: error?.fields,
+    });
+  }
+
   res.status(statusCode || HttpStatus.INTERNAL_SERVER_ERROR).json({
     status,
     statusCode: statusCode || HttpStatus.INTERNAL_SERVER_ERROR,
